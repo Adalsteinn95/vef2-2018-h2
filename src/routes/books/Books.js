38e24b05e98d0cff8d6ef21c43d62f94ef6f309d
Fannar <fannargauti@gmail.com>
2018-04-10 14:01:47 +0000

    added offset parameter functionality

--- src/routes/books/Books.js
+++ src/routes/books/Books.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import queryString from "query-string";
 import { fetchBooks } from "../../actions/books";
 import Button from "../../components/button";
 
@@ -8,32 +10,36 @@ import Button from "../../components/button";
 import "./Books.css";
 
 class Books extends Component {
-  state = { offset: 0 };
+  urlOffset = queryString.parse(this.props.location.search).offset;
+  state = {
+    offset: this.urlOffset > 0 ? this.urlOffset : 0
+  };
 
   async componentDidMount() {
     this.fetchBooks();
   }
 
-  async fetchBooks() {
+  async fetchBooks(endpoint) {
     this.props.dispatch(fetchBooks(`books?offset=${this.state.offset}`));
+    this.props.history.push(`?offset=${this.state.offset}`);
   }
 
-  handleNextPageClick = e => {
+  handlePageClick = key => {
     this.setState((prevState, props) => {
-      return { offset: prevState.offset + 10 };
-    }, this.fetchBooks);
-  };
-  handlePrevPageClick = e => {
-    this.setState((prevState, props) => {
+      console.log(prevState.offset === 0 && key === "prev");
+      console.log(prevState.offset + (key === "prev" ? -10 : 10));
       return {
         offset:
-          prevState.offset === 0 ? prevState.offset : prevState.offset - 10
+          prevState.offset === 0 && key === "prev"
+            ? prevState.offset
+            : Number(prevState.offset) + (key === "prev" ? -10 : 10)
       };
     }, this.fetchBooks);
   };
   render() {
-    const { books, isFetching, message } = this.props;
-
+    console.log(this.props);
+    const { books: booksData, isFetching, message } = this.props;
+    const { books } = booksData;
     if (isFetching) {
       return <div>Sæki gögn...</div>;
     }
@@ -46,7 +52,7 @@ class Books extends Component {
       <div>
         <h2>Bækur</h2>
         <div key={this.state.offset}>
-          {books.books.items.map(book => {
+          {books.items.map(book => {
             return (
               <div key={book.id}>
                 <h3>{book.title}</h3>
@@ -55,8 +61,14 @@ class Books extends Component {
             );
           })}
         </div>
-        <Button onClick={this.handlePrevPageClick} children="Fyrri síða" />
-        <Button onClick={this.handleNextPageClick} children="Næsta síða" />
+        <Button
+          onClick={() => this.handlePageClick("prev")}
+          children={"Fyrri síða"}
+        />
+        <Button
+          onClick={() => this.handlePageClick("next")}
+          children={"Næsta síða"}
+        />
       </div>
     );
   }
